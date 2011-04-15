3700dc1e15493addfe00586f57f1efcdd7e05bc8
Ganesan <minigane@gmail.com>
2011-04-15 10:23:55 +0900

    Generated the Rails 3 Application

---
+++ cucumber-intro/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CucumberIntro::Application.config.secret_token = '559ca845d2dbd4fdfbda8aaaf30691896e92602e841a315de9ca9b85462777456ab8f7fd0ee2580e97f524f8a38d094e87376c946da9cf47decfb06c9eb8cef8'
---
+++ things_i_bought/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '889fd6e44c3158c76c8f9eb0b2cddd6aa9847fb86884803f0e736b6d256d2be66ed605ba5f43e89c5e5a698e8b672b426c26c0470437bc9e4b52fe9463332cf5'
