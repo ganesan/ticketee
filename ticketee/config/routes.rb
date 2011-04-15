1c1a3c40f72bd687878a8350ab2ef772d36cbe45
Ganesan <minigane@gmail.com>
2011-04-15 12:38:04 +0900

    Make changes on controller from Project to Projects

--- ticketee/config/routes.rb
+++ ticketee/config/routes.rb
@@ -46,12 +46,16 @@ Ticketee::Application.routes.draw do
   #     resources :products
   #   end
 
+
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
     root :to => "projects#index"
   # See how all your routes lay out with "rake routes"
+  
 
+  resources :projects
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
