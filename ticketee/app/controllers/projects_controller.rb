a047b888ed2c84b33922e1e86ae2b4518febf39d
Ganesan <minigane@gmail.com>
2011-04-22 09:23:49 +0900

    Implemented creating tickets for a project

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
   
   def index
     @projects = Project.all
@@ -46,5 +46,12 @@ class ProjectsController < ApplicationController
     redirect_to projects_path
   end
   
+  private 
+    def find_project
+      @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking for could not be found."
+      redirect_to projects_path
+    end
   
 end
